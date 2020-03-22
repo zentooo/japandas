9973ae40a2e71cc5e5aa6ef3741e31ad8946a92d
zentooo <ankerasoy@gmail.com>
2020-03-22 10:24:52 +0900

    removed

--- japandas/__init__.py
+++ japandas/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import japandas.core.strings                                                          # noqa
-import japandas.io.data                                                               # noqa
-from japandas.io.data import DataReader                                               # noqa
 from japandas.tseries.tools import to_datetime, date_range, period_range              # noqa
 from japandas.tseries.holiday import JapaneseHolidayCalendar, TSEHolidayCalendar      # noqa
-import japandas.tools.plotting                                                        # noqa
 
 from japandas.version import version as __version__                                   # noqa
